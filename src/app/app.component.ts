a2c7d49c7a071269b739dbfe885be4978730c71c
Leo Velez <leonardo.velez@kaplan.edu>
2023-03-10 13:02:21 -0500

    CY_OPS inspired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,43 +6,12 @@ import { AfterViewInit, Component, ElementRef, Inject, Renderer2, ViewChild } fr
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild('cards', {static: false}) cardEl: ElementRef | undefined;
-  @ViewChild('backgroundTitle', {static: false}) bTitleEl: ElementRef | undefined;
-  @ViewChild('overlayTitle', {static: false}) oTitleEl: ElementRef | undefined;
-
-  currTheme = 'cmykill_em_all';
-  allThemes = [
-    'cmykill_em_all',
-    'retro',
-    'omen',
-    'low_power',
-    'mork',
-  ];
-
   constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private renderer: Renderer2
   ) {}
 
   ngAfterViewInit(): void {
-      const theme = window.localStorage.getItem('fuller-auto-theme');
-      if (theme) {
-        this.currTheme = theme;
-      }
-      console.log(this.currTheme);
-      // this.applyTheme();
   }
 
   shuffleTheme() {
-    const themesLength = this.allThemes.length - 1;
-    const nextIndex = this.allThemes.indexOf(this.currTheme) + 1;
-
-    if (themesLength < nextIndex) {
-      this.currTheme = this.allThemes[0];
-    } else {
-      this.currTheme = this.allThemes[nextIndex];
-    }
-    window.localStorage.setItem('fuller-auto-theme', this.currTheme);
-    // this.applyTheme();
   }
 }
